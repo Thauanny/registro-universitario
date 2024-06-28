33e96f0e14de1d1238fb895af4db5122cf66cc85
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2024-06-27 22:50:10 -0300

    explicitando rotas admin no filtro

--- src/main/java/com/universidade/registro_universidade/infra/security/SecurityConfiguration.java
+++ src/main/java/com/universidade/registro_universidade/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.universidade.registro_universidade.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +19,23 @@ public class SecurityConfiguration {
       .csrf(csrf -> csrf.disable())
       .sessionManagement(session ->
         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      )
+      .authorizeHttpRequests(authorize -> authorize
+      .requestMatchers(HttpMethod.POST, "professor/register" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "professor/deleteLogic" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "professor" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.DELETE, "professor" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.POST, "aluno/register" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "aluno/deleteLogic" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "aluno" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.DELETE, "aluno" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.POST, "turma/register" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "turma/addProfessor" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "turma/addAluno" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "turma/deleteLogic" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "turma" ).hasRole("ADMIN")
+      .requestMatchers(HttpMethod.DELETE, "turma" ).hasRole("ADMIN")
+      .anyRequest().authenticated()
       )
       .build();
   }
