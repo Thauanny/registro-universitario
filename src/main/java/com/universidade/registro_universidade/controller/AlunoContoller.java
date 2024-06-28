86f039dbb8134352aba3644ba92715ca387f4f49
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2024-06-28 20:29:03 -0300

    retirando register alunocontroller

--- src/main/java/com/universidade/registro_universidade/controller/AlunoContoller.java
+++ src/main/java/com/universidade/registro_universidade/controller/AlunoContoller.java
@@ -1,6 +1,5 @@
 package com.universidade.registro_universidade.controller;
 
-import com.universidade.registro_universidade.DTO.AlunoCreateDTO;
 import com.universidade.registro_universidade.DTO.AlunoDTO;
 import com.universidade.registro_universidade.service.AlunoService;
 import jakarta.persistence.EntityNotFoundException;
@@ -16,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,11 +62,6 @@ public class AlunoContoller {
         .status(HttpStatus.BAD_REQUEST)
         .body("{\"message\": \"Algo deu errado\"}");
     }
-  }
-
-  @PostMapping("/register")
-  public ResponseEntity<?> register(@RequestBody AlunoCreateDTO aluno) {
-    return ResponseEntity.badRequest().build();
   }
 
   @PutMapping("/{id}")
